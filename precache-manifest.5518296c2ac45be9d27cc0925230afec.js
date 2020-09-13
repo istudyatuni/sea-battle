297cabb5e10f1752f05717d21038b558dd7f5da1
Ilia <fortyhell@gmail.com>
2020-09-13 15:17:43 +0300

    Updates

---
+++ precache-manifest.5518296c2ac45be9d27cc0925230afec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bf8385dc7ac23bb4fc34518708377cd",
+    "url": "/test-deploy-only-one-branch/index.html"
+  },
+  {
+    "revision": "0c2e4a8b71cb34e9dc7b",
+    "url": "/test-deploy-only-one-branch/static/css/main.10a6414b.chunk.css"
+  },
+  {
+    "revision": "fb1e56dec6f4c65f9909",
+    "url": "/test-deploy-only-one-branch/static/js/2.81c19146.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-deploy-only-one-branch/static/js/2.81c19146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c2e4a8b71cb34e9dc7b",
+    "url": "/test-deploy-only-one-branch/static/js/main.c0cd8326.chunk.js"
+  },
+  {
+    "revision": "8015c2941f4f050aa1d9",
+    "url": "/test-deploy-only-one-branch/static/js/runtime-main.d13b8e21.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sea-battle/precache-manifest.c8a3f9942f9b457d86ce2472df1e4597.js"
+  "/test-deploy-only-one-branch/precache-manifest.5518296c2ac45be9d27cc0925230afec.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sea-battle/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test-deploy-only-one-branch/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
