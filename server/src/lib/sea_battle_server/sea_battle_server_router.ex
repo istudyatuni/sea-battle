093bf7357cd075c9ce6df41bf3be1b0c73b4aaf7
Ilia <fortyhell@gmail.com>
2020-09-21 08:30:45 +0300

    Move functions to handler
    Add normal response and HTTP codes

--- server/src/lib/sea_battle_server/sea_battle_server_router.ex
+++ server/src/lib/sea_battle_server/sea_battle_server_router.ex
@@ -11,41 +11,24 @@ defmodule SeaBattleServer.Router do
   plug(:match)
   plug(:dispatch)
 
-  @all_ships :all_ships
-
-  def insert_new_ships(ships) do
-    existance = :ets.insert_new(@all_ships, {ships["id"], ships["ships"]})
-
-    if existance == false do
-      Logger.debug("ships table already exist, id=#{ships["id"]}")
-    else
-      Logger.debug("inserting . .")
-    end
-  end
-
-  def show_ships(id) do
-    IO.puts("show..")
-    # if table not exist
-    case :ets.whereis(@all_ships) do
-      :undefined -> IO.puts("ships is undefined")
-      _ -> IO.inspect(:ets.lookup(@all_ships, id))
-    end
-  end
-
   get "/shot" do
     {:ok, body, conn} = read_body(conn)
     body = Poison.decode!(body)
 
     ans = ShipHandler.hitOrMiss(body["id"], body["x"], body["y"])
-    responce = Poison.encode!(ans, [])
-    send_resp(conn, 200, responce)
+    ans = %{"id" => body["id"], "type" => ans}
+
+    response = Poison.encode!(ans, [])
+    send_resp(conn, 200, response)
   end
 
   post "/ships" do
     {:ok, body, conn} = read_body(conn)
     body = Poison.decode!(body)
-    insert_new_ships(body)
-    send_resp(conn, 201, "created")
+
+    code = ShipHandler.insert_new_ships(body)
+    ShipHandler.show_ships(body["id"])
+    send_resp(conn, code, "")
   end
 
   # "Default" route that will get called when no other route is matched
--- server/src/lib/sea_battle_server/ship_handler.ex
+++ server/src/lib/sea_battle_server/ship_handler.ex
@@ -8,12 +8,38 @@ defmodule SeaBattleServer.ShipHandler do
     end
   end
 
+  def insert_new_ships(ships) do
+    existance = :ets.insert_new(@all_ships, {ships["id"], ships["ships"]})
+
+    if existance == false do
+      Logger.debug("ships table already exist, id=#{ships["id"]}")
+      # not modified
+      _ = 304
+    else
+      Logger.debug("inserting . .")
+      # created
+      _ = 201
+    end
+  end
+
+  def show_ships(id) do
+    IO.puts("show..")
+    # if table not exist
+    case :ets.whereis(@all_ships) do
+      :undefined -> Logger.debug("ships is undefined")
+      _ -> IO.inspect(:ets.lookup(@all_ships, id))
+    end
+  end
+
   def hitOrMiss(id, x, y) do
     # 1st elem from table's head (hd)
-    ships = elem(hd(:ets.lookup(@all_ships, id)), 1)
+    ships = hd(:ets.lookup(@all_ships, id))
+    ships = elem(ships, 1)
+
     value = Enum.at(ships, x)
     value = Enum.at(value, y)
 
+    Logger.debug("player is shot, x=#{x}, y=#{y}, value=#{value}")
     if value == 1 do
       _ = "hit"
     else
