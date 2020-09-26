9fa52e20356c2708f17131ff27d540cc10bbfb14
Ilia <fortyhell@gmail.com>
2020-09-26 19:10:01 +0300

    useEffect instead Cell reacting on prop change

--- web/src/components/Battlefield/Cell.tsx
+++ web/src/components/Battlefield/Cell.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import {
   settingShips,
@@ -15,6 +15,10 @@ type CellProps = {
 const Cell: React.FC<CellProps> = ({ i, isClear, gameMode, setShip }) => {
   // first == true when first cell's render
 
+  useEffect(()=>{
+    setImg(fillBlock(true))
+  },[isClear]);
+
   function fillBlock(first: boolean): object {
     let path
     if(gameMode===0){
@@ -23,11 +27,21 @@ const Cell: React.FC<CellProps> = ({ i, isClear, gameMode, setShip }) => {
         path = settingShips(first, isClear)
         setShip(i)
       } else {
+        // here instead isClear set param for
+        // detecting hit/miss
+        // and move it away
+        if(isClear===false) {
+          path = "Hit"
+        } else {
+          path = "Empty"
+        }
+
         path = "Empty"
       }
     } else if(gameMode===1) {
       // when battle
       if(first===false) {
+        // when player tap to cell
         path = battle(1)
       } else {
         // before battle need clear field
